c4d7563bc67f4d35f50648de918717af0f2deb6f
Yuval Deutscher <yuval@deut.sh>
2020-08-01 11:30:59 +0300

    added independent mode

--- hasalary.py
+++ hasalary.py
@@ -13,6 +13,8 @@ import argparse
 AVERAGE_SALARY = 10551
 STUDY_FUND_EMPLOYEE = 0.025
 STUDY_FUND_EMPLOYER = 0.075
+STUDY_FUND_INDEPENDENT = 0.045
+STUDY_FUND_INDEPENDENT_MAX_SALARY = 265000 / 12
 
 ### Income Tax law ###
 INCOME_TAX_POINT_WORTH = 2628 / 12  # Section 33a
@@ -37,6 +39,11 @@ PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX = 34900 / 12  # Section 3(e3)(2)
 STUDY_FUND_TAX_EXEMPT_MAX = 15712  # Section 3(e)
 REPARATIONS_PULL_TAX_EXEMPT_MAX = 12420 / 12  # Section 7a(a)(2)
 
+PENSION_INDEPENDENT_RATE_A = 0.11
+PENSION_INDEPENDENT_RATE_A_MAX = 23232 / 12
+PENSION_INDEPENDENT_RATE_B = 0.055
+PENSION_INDEPENDENT_RATE_B_MAX = 11616 / 12
+
 ### National Insurance law ###
 NATIONAL_INSURANCE_STEPS = [
     (AVERAGE_SALARY * 0.6, 0.004),
@@ -51,6 +58,11 @@ HEALTH_INSURANCE_STEPS = [
 EMPLOYER_NATIONAL_INSURANCE_STEPS = [
     (AVERAGE_SALARY * 0.6, 0.0355),
     (44020, 0.076)
+]
+
+INDEPENDENT_NATIONAL_INSURANCE_STEPS = [
+    (AVERAGE_SALARY * 0.6, 0.0287),
+    (44020, 0.1283)
 ]
 
 
@@ -82,38 +94,62 @@ def main():
         exec(f.read(), d)
         globals().update(d)
 
-    salary = base_salary * percentage + travel_allowance + bonuses
-    tax_worth_features = ten_bis + goods
+    if independent_mode:
+        salary = base_salary * percentage
+        social_salary = salary - tax_worth_expenses
+        salary_for_natins = salary - tax_worth_expenses
 
-    # Social payments
-    social_salary = base_salary * percentage
-    pens = PENSION_EMPLOYEE * social_salary
-    if social_salary > PENSION_EMPLOYER_TAX_EXEMPT_SALARY_MAX:
-        # Zkifat Tagmulim
-        tax_worth_features += (social_salary -
-                               PENSION_EMPLOYER_TAX_EXEMPT_SALARY_MAX) * PENSION_EMPLOYER
-    reparations = PENSION_REPERATIONS * social_salary
-    if reparations > PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX:
-        # Zkifat Pitzuiim
-        tax_worth_features += reparations - PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX
-    if social_salary > STUDY_FUND_TAX_EXEMPT_MAX and full_study_fund:
-        # Zkifat Hishtalmut
-        tax_worth_features += (social_salary -
-                               STUDY_FUND_TAX_EXEMPT_MAX) * STUDY_FUND_EMPLOYER
-        sfund = STUDY_FUND_EMPLOYEE * social_salary
+        # Social paymens
+        pens_a = min(PENSION_INDEPENDENT_RATE_A * social_salary,
+                     PENSION_INDEPENDENT_RATE_A_MAX)
+        pens_b = min(PENSION_INDEPENDENT_RATE_B * social_salary,
+                     PENSION_INDEPENDENT_RATE_B_MAX)
+        pens = pens_a + pens_b
+        sfund = STUDY_FUND_INDEPENDENT * \
+            min(social_salary, STUDY_FUND_INDEPENDENT_MAX_SALARY)
+
+        # National insurance
+        natins_tax = tax_steps(
+            salary_for_natins, INDEPENDENT_NATIONAL_INSURANCE_STEPS)
+        healthins_tax = tax_steps(salary_for_natins, HEALTH_INSURANCE_STEPS)
+
+        # Income tax
+        salary_for_income = salary - tax_worth_expenses - pens_a - sfund
+        in_tax = income_tax(salary_for_income, tax_pts)
+        in_tax -= PENSION_REIMBURSE * pens_b
     else:
-        sfund = STUDY_FUND_EMPLOYEE * \
-            min(social_salary, STUDY_FUND_TAX_EXEMPT_MAX)
+        salary = base_salary * percentage + travel_allowance + bonuses
+        tax_worth_features = ten_bis + goods
 
-    # National Insurance
-    salary_for_natins = salary + tax_worth_features
-    natins_tax = tax_steps(salary_for_natins, NATIONAL_INSURANCE_STEPS)
-    healthins_tax = tax_steps(salary_for_natins, HEALTH_INSURANCE_STEPS)
+        # Social payments
+        social_salary = base_salary * percentage
+        pens = PENSION_EMPLOYEE * social_salary
+        if social_salary > PENSION_EMPLOYER_TAX_EXEMPT_SALARY_MAX:
+            # Zkifat Tagmulim
+            tax_worth_features += (social_salary -
+                                   PENSION_EMPLOYER_TAX_EXEMPT_SALARY_MAX) * PENSION_EMPLOYER
+        reparations = PENSION_REPERATIONS * social_salary
+        if reparations > PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX:
+            # Zkifat Pitzuiim
+            tax_worth_features += reparations - PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX
+        if social_salary > STUDY_FUND_TAX_EXEMPT_MAX and full_study_fund:
+            # Zkifat Hishtalmut
+            tax_worth_features += (social_salary -
+                                   STUDY_FUND_TAX_EXEMPT_MAX) * STUDY_FUND_EMPLOYER
+            sfund = STUDY_FUND_EMPLOYEE * social_salary
+        else:
+            sfund = STUDY_FUND_EMPLOYEE * \
+                min(social_salary, STUDY_FUND_TAX_EXEMPT_MAX)
 
-    # Income Tax
-    salary_for_income = salary + tax_worth_features - tax_worth_expenses
-    in_tax = income_tax(salary_for_income, tax_pts)
-    in_tax -= PENSION_REIMBURSE * min(pens, PENSION_REIMBURSE_PAYMENTS_MAX)
+        # National Insurance
+        salary_for_natins = salary + tax_worth_features
+        natins_tax = tax_steps(salary_for_natins, NATIONAL_INSURANCE_STEPS)
+        healthins_tax = tax_steps(salary_for_natins, HEALTH_INSURANCE_STEPS)
+
+        # Income Tax
+        salary_for_income = salary + tax_worth_features - tax_worth_expenses
+        in_tax = income_tax(salary_for_income, tax_pts)
+        in_tax -= PENSION_REIMBURSE * min(pens, PENSION_REIMBURSE_PAYMENTS_MAX)
 
     # Part 1 (paycheck)
     netto_salary = salary - in_tax - natins_tax - healthins_tax - pens - sfund
@@ -131,41 +167,45 @@ def main():
     print("-------------------------------------")
 
     # Part 2 (total income)
-    reparations_cash = min(reparations, REPARATIONS_PULL_TAX_EXEMPT_MAX)
-    if reparations > PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX:
-        reparations_cash += reparations - PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX
-    employer_sfund = STUDY_FUND_EMPLOYER * \
-        (social_salary if full_study_fund else min(
-            social_salary, STUDY_FUND_TAX_EXEMPT_MAX))
+    if independent_mode:
+        reparations_cash = 0
+        employer_sfund = 0
+    else:
+        reparations_cash = min(reparations, REPARATIONS_PULL_TAX_EXEMPT_MAX)
+        if reparations > PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX:
+            reparations_cash += reparations - PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX
+        employer_sfund = STUDY_FUND_EMPLOYER * \
+            (social_salary if full_study_fund else min(
+                social_salary, STUDY_FUND_TAX_EXEMPT_MAX))
     sfund_cash = sfund + employer_sfund
     total_monthly_income = netto_salary + reparations_cash + sfund_cash
-    total_monthly_income = postprocess(total_monthly_income)
+    total_monthly_income2 = postprocess(total_monthly_income)
     print("---Other stats---")
     print(f"Reparations cash: {round(reparations_cash)}")
     print(f"Study fund cash: {round(sfund_cash)}")
     print(f"Total income: {round(total_monthly_income)}")
+    if total_monthly_income != total_monthly_income2:
+        print(f"Total income (post): {round(total_monthly_income2)}")
+        total_monthly_income = total_monthly_income2
 
     # Part 3 (savings)
-    if target is None:
-        return
-    monthly_gain = total_monthly_income - monthly_expense
-    monthly_gain_rate = 1 + (yearly_gain_rate - 1) / 12
-    for i in itertools.count():
-        if current_cash * monthly_gain_rate ** i + sum(monthly_gain * monthly_gain_rate ** j for j in range(i)) >= target:
-            years = i / 12
-            break
-    print(f"{years:.1f} years to reach target of {round(target)} with {round(monthly_gain)} monthly saving")
+    if target is not None:
+        monthly_gain = total_monthly_income - monthly_expense
+        monthly_gain_rate = 1 + (yearly_gain_rate - 1) / 12
+        for i in itertools.count():
+            if current_cash * monthly_gain_rate ** i + sum(monthly_gain * monthly_gain_rate ** j for j in range(i)) >= target:
+                years = i / 12
+                break
+        print(f"{years:.1f} years to reach target of {round(target)} with {round(monthly_gain)} monthly saving")
 
     # Part 4 (employment cost)
-    if not calculate_employment_cost:
-        return
-
-    employer_pension = PENSION_EMPLOYER * social_salary
-    employer_natins = tax_steps(
-        salary_for_natins, EMPLOYER_NATIONAL_INSURANCE_STEPS)
-    employment_cost = salary + ten_bis + goods + employer_natins + \
-        employer_pension + reparations + employer_sfund
-    print(f"Employment cost: {round(employment_cost)}")
+    if not independent_mode and calculate_employment_cost:
+        employer_pension = PENSION_EMPLOYER * social_salary
+        employer_natins = tax_steps(
+            salary_for_natins, EMPLOYER_NATIONAL_INSURANCE_STEPS)
+        employment_cost = salary + ten_bis + goods + employer_natins + \
+            employer_pension + reparations + employer_sfund
+        print(f"Employment cost: {round(employment_cost)}")
 
 
 if __name__ == "__main__":
