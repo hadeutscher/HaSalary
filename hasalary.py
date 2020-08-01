779938fcf3389271fcb0314ad0c392d73177f605
Yuval Deutscher <yuval@deut.sh>
2020-08-01 10:25:25 +0300

    code formatting

--- hasalary.py
+++ hasalary.py
@@ -15,7 +15,7 @@ STUDY_FUND_EMPLOYEE = 0.025
 STUDY_FUND_EMPLOYER = 0.075
 
 ### Income Tax law ###
-INCOME_TAX_POINT_WORTH = 2628 / 12 # Section 33a
+INCOME_TAX_POINT_WORTH = 2628 / 12  # Section 33a
 
 # Section 121
 INCOME_TAX_STEPS = [
@@ -26,30 +26,33 @@ INCOME_TAX_STEPS = [
     (504360 / 12, 0.35),
     (651600 / 12, 0.47),
     (math.inf, 0.50)
-    ]
+]
 
-PENSION_REIMBURSE = 0.35 # Section 45a(b)
-PENSION_REIMBURSE_PAYMENTS_MAX = 105600 * 0.07 / 12 # Section 47(a)(1), also see 45a(d)(2)(b)(2), 45a(e)(2)(b)(2)(a)
-PENSION_EMPLOYER_TAX_EXEMPT_SALARY_MAX = AVERAGE_SALARY * 2.5 # Section 3(e3)(2)
-PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX = 34900 / 12 # Section 3(e3)(2)
-STUDY_FUND_TAX_EXEMPT_MAX = 15712 # Section 3(e)
-REPARATIONS_PULL_TAX_EXEMPT_MAX = 12420 / 12 # Section 7a(a)(2)
+PENSION_REIMBURSE = 0.35  # Section 45a(b)
+# Section 47(a)(1), also see 45a(d)(2)(b)(2), 45a(e)(2)(b)(2)(a)
+PENSION_REIMBURSE_PAYMENTS_MAX = 105600 * 0.07 / 12
+PENSION_EMPLOYER_TAX_EXEMPT_SALARY_MAX = AVERAGE_SALARY * \
+    2.5  # Section 3(e3)(2)
+PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX = 34900 / 12  # Section 3(e3)(2)
+STUDY_FUND_TAX_EXEMPT_MAX = 15712  # Section 3(e)
+REPARATIONS_PULL_TAX_EXEMPT_MAX = 12420 / 12  # Section 7a(a)(2)
 
 ### National Insurance law ###
 NATIONAL_INSURANCE_STEPS = [
     (AVERAGE_SALARY * 0.6, 0.004),
     (44020, 0.07)
-    ]
+]
 
 HEALTH_INSURANCE_STEPS = [
     (AVERAGE_SALARY * 0.6, 0.031),
     (44020, 0.05)
-    ]
+]
 
 EMPLOYER_NATIONAL_INSURANCE_STEPS = [
     (AVERAGE_SALARY * 0.6, 0.0355),
     (44020, 0.076)
-    ]
+]
+
 
 def tax_steps(s, steps):
     tax = 0
@@ -65,7 +68,9 @@ def tax_steps(s, steps):
 def income_tax(s, pts):
     return max(tax_steps(s, INCOME_TAX_STEPS) - (INCOME_TAX_POINT_WORTH * pts), 0)
 
-postprocess = lambda x: x
+
+def postprocess(x): return x
+
 
 def main():
     # Loading config
@@ -85,17 +90,20 @@ def main():
     pens = PENSION_EMPLOYEE * social_salary
     if social_salary > PENSION_EMPLOYER_TAX_EXEMPT_SALARY_MAX:
         # Zkifat Tagmulim
-        tax_worth_features += (social_salary - PENSION_EMPLOYER_TAX_EXEMPT_SALARY_MAX) * PENSION_EMPLOYER
+        tax_worth_features += (social_salary -
+                               PENSION_EMPLOYER_TAX_EXEMPT_SALARY_MAX) * PENSION_EMPLOYER
     reparations = PENSION_REPERATIONS * social_salary
     if reparations > PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX:
         # Zkifat Pitzuiim
         tax_worth_features += reparations - PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX
     if social_salary > STUDY_FUND_TAX_EXEMPT_MAX and full_study_fund:
         # Zkifat Hishtalmut
-        tax_worth_features += (social_salary - STUDY_FUND_TAX_EXEMPT_MAX) * STUDY_FUND_EMPLOYER
+        tax_worth_features += (social_salary -
+                               STUDY_FUND_TAX_EXEMPT_MAX) * STUDY_FUND_EMPLOYER
         sfund = STUDY_FUND_EMPLOYEE * social_salary
     else:
-        sfund = STUDY_FUND_EMPLOYEE * min(social_salary, STUDY_FUND_TAX_EXEMPT_MAX)
+        sfund = STUDY_FUND_EMPLOYEE * \
+            min(social_salary, STUDY_FUND_TAX_EXEMPT_MAX)
 
     # National Insurance
     salary_for_natins = salary + tax_worth_features
@@ -110,11 +118,15 @@ def main():
     # Part 1 (paycheck)
     netto_salary = salary - in_tax - natins_tax - healthins_tax - pens - sfund
     print("---Paycheck details---")
-    print(f"Income tax: {round(in_tax)} from a salary of {round(salary_for_income)} with {tax_pts} points")
-    print(f"National Insurance: {round(natins_tax)} from a salary of {round(salary_for_natins)}")
-    print(f"Health Insurance: {round(healthins_tax)} from a salary of {round(salary_for_natins)}")
+    print(
+        f"Income tax: {round(in_tax)} from a salary of {round(salary_for_income)} with {tax_pts} points")
+    print(
+        f"National Insurance: {round(natins_tax)} from a salary of {round(salary_for_natins)}")
+    print(
+        f"Health Insurance: {round(healthins_tax)} from a salary of {round(salary_for_natins)}")
     print(f"Pension: {round(pens)} from a salary of {round(social_salary)}")
-    print(f"Study Fund: {round(sfund)} from a salary of {round(social_salary)}")
+    print(
+        f"Study Fund: {round(sfund)} from a salary of {round(social_salary)}")
     print(f"Salary (Net): {round(netto_salary)}")
     print("-------------------------------------")
 
@@ -122,7 +134,9 @@ def main():
     reparations_cash = min(reparations, REPARATIONS_PULL_TAX_EXEMPT_MAX)
     if reparations > PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX:
         reparations_cash += reparations - PENSION_REPARATIONS_TAX_EXEMPT_SALARY_MAX
-    employer_sfund = STUDY_FUND_EMPLOYER * (social_salary if full_study_fund else min(social_salary, STUDY_FUND_TAX_EXEMPT_MAX))
+    employer_sfund = STUDY_FUND_EMPLOYER * \
+        (social_salary if full_study_fund else min(
+            social_salary, STUDY_FUND_TAX_EXEMPT_MAX))
     sfund_cash = sfund + employer_sfund
     total_monthly_income = netto_salary + reparations_cash + sfund_cash
     total_monthly_income = postprocess(total_monthly_income)
@@ -145,12 +159,14 @@ def main():
     # Part 4 (employment cost)
     if not calculate_employment_cost:
         return
-    
+
     employer_pension = PENSION_EMPLOYER * social_salary
-    employer_natins = tax_steps(salary_for_natins, EMPLOYER_NATIONAL_INSURANCE_STEPS)
-    employment_cost = salary + ten_bis + goods + employer_natins + employer_pension + reparations + employer_sfund
+    employer_natins = tax_steps(
+        salary_for_natins, EMPLOYER_NATIONAL_INSURANCE_STEPS)
+    employment_cost = salary + ten_bis + goods + employer_natins + \
+        employer_pension + reparations + employer_sfund
     print(f"Employment cost: {round(employment_cost)}")
-    
+
 
 if __name__ == "__main__":
     main()
